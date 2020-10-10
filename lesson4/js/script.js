79ac29034dc9976ae40d497bcf085c31be319eef
Kemblesque <beau.c.kemble@gmail.com>
2020-10-10 13:13:57 -0600

    nav

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,2 +1,6 @@
+function toggleMenu() {
+    document.getElementsByClassName("nav_bar")[0].classList.toggle("responsive");
+  }
+
 document.getElementById("updated").innerHTML = document.lastModified
 document.getElementById("currentYear").textContent = new Date().getFullYear();
